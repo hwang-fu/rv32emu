b31892be0e6ec887177e678029c3bafb26975d79
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-29 17:54:14 +0100

    feat: add is_within_bounds()

--- lib/memory.ml
+++ lib/memory.ml
@@ -6,3 +6,9 @@ type t =
 
 (** Create memory of given size (in bytes), initialized to zero *)
 let create ~size = { data = Bytes.make size '\x00'; size }
+
+let is_within_bounds memory address =
+  let addr_start = Int32.to_int address in
+  if addr_start < 0 || addr_start >= memory.size
+  then failwith (Printf.sprintf "Memory access out of bounds: 0x%08lx" address)
+;;
