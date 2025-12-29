757e5e77936b76e21e0c5414b610efd8f6d09e13
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-29 18:11:40 +0100

    test: add test_sign_extend()

--- test/test_bits.ml
+++ test/test_bits.ml
@@ -30,3 +30,18 @@ let test_extract_bits () =
   test "extract [0:0] from 0x01" (Bits.extract_bits 0x01l ~hi:0 ~lo:0 = 1l);
   test "extract [0:0] from 0x00" (Bits.extract_bits 0x00l ~hi:0 ~lo:0 = 0l)
 ;;
+
+(** Test sign_extend *)
+let test_sign_extend () =
+  print_endline "Testing sign_extend:";
+  (* Positive 8-bit value stays positive *)
+  test "sign_extend 0x7F from 8 bits" (Bits.sign_extend 0x7Fl ~bits:8 = 0x7Fl);
+  (* Negative 8-bit value (0x80 = -128) extends to 0xFFFFFF80 *)
+  test "sign_extend 0x80 from 8 bits" (Bits.sign_extend 0x80l ~bits:8 = 0xFFFFFF80l);
+  (* 0xFF as 8-bit = -1, extends to 0xFFFFFFFF *)
+  test "sign_extend 0xFF from 8 bits" (Bits.sign_extend 0xFFl ~bits:8 = 0xFFFFFFFFl);
+  (* 16-bit positive *)
+  test "sign_extend 0x7FFF from 16 bits" (Bits.sign_extend 0x7FFFl ~bits:16 = 0x7FFFl);
+  (* 16-bit negative *)
+  test "sign_extend 0x8000 from 16 bits" (Bits.sign_extend 0x8000l ~bits:16 = 0xFFFF8000l)
+;;
