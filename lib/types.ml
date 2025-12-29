cc5adb49854ee62ee7ee5dc0b9198187002eb55c
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-29 17:05:51 +0100

    feat: add type word

---
+++ lib/types.ml
@@ -0,0 +1,4 @@
+(** 32-bit word - the fundamental unit in RV32.
+      We use int32 because OCaml's int is 63-bit on 64-bit systems,
+      and we need exact 32-bit wraparound semantics for arithmetic. *)
+type word = int32
