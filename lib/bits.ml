95dfa4f3d078e447e3b267331fc47c281a2be175
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-29 17:14:56 +0100

    feat: add is_bit_set()

--- lib/bits.ml
+++ lib/bits.ml
@@ -5,3 +5,9 @@ let set_bit word position = Int32.logor word (Int32.shift_left 1l position)
 let clr_bit word position =
   Int32.logand word (Int32.lognot (Int32.shift_left 1l position))
 ;;
+
+(** Test if a specific bit is set (1).
+    Bit positions are 0-indexed from the right. *)
+let is_bit_set word position =
+  Int32.logand (Int32.shift_right_logical word position) 1l = 1l
+;;
