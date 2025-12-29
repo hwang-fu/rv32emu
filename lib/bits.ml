b26d2627c68ea97f5aaed9875a47379abf2c2276
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-29 17:14:09 +0100

    feat: add clr_bit()

--- lib/bits.ml
+++ lib/bits.ml
@@ -1,2 +1,7 @@
 (** Set a specific bit to 1. *)
 let set_bit word position = Int32.logor word (Int32.shift_left 1l position)
+
+(** Clear a specific bit to 0. *)
+let clr_bit word position =
+  Int32.logand word (Int32.lognot (Int32.shift_left 1l position))
+;;
