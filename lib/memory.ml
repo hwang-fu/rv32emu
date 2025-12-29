4e7a83e54e6766a0094b2abeb0d516dd6e01f6f5
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-29 17:54:25 +0100

    feat: add load_byte()

--- lib/memory.ml
+++ lib/memory.ml
@@ -7,8 +7,15 @@ type t =
 (** Create memory of given size (in bytes), initialized to zero *)
 let create ~size = { data = Bytes.make size '\x00'; size }
 
+(** Check if address is within bounds *)
 let is_within_bounds memory address =
   let addr_start = Int32.to_int address in
   if addr_start < 0 || addr_start >= memory.size
   then failwith (Printf.sprintf "Memory access out of bounds: 0x%08lx" address)
+;;
+
+(** Load a single byte (unsigned, 0-255) *)
+let load_byte memory address =
+  is_within_bounds memory address;
+  Char.code (Bytes.get memory.data (Int32.to_int address))
 ;;
